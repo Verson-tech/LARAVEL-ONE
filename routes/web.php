a7a5abd9f782e0799dce3be5d23486dd1cd4f437
Vira Murray <vira.avramenko@gmail.com>
2024-04-24 15:09:15 -0400

    sample route https://laravel.com/docs/11.x/routing

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/about', function () {
-    return "About page";
-});
+// Route::get('/about', function () {
+//     return "About page";
+// });
 
-Route::get('/contact', function () {
-    return "Contact page";
-});
+// Route::get('/contact', function () {
+//     return "Contact page";
+// });
 
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This post id is " . $id . " and name is " . $name;
-});
+// //multiple parameters in the route
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "This post id is " . $id . " and name is " . $name;
+// });
+
+// //naming routes
+// Route::get('/admin/posts/example', array('as' => 'admin.home', function () {
+//     $url = route('admin.home');
+//     return "This url is " . $url;
+// }));
+
+Route::get('/post', [PostsController::class, 'index']);
