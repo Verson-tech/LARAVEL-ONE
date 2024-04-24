39090f12af788bec429c4d686772da9e594b30da
Vira Murray <vira.avramenko@gmail.com>
2024-04-24 14:01:22 -0400

    multiple parameters in the route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return "Contact page";
 });
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "This post id is " . $id . " and name is " . $name;
+});
